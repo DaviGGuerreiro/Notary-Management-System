6768da39201776be2aa8df92c2c2589ea59bbfb7
Davi Guerreiro <daviguerreiro9a@gmail.com>
2026-05-27 08:06:12 -0300

    feat: Server definition and first structure

---
+++ backend/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const app = express();
+app.use(cors()) //permissao de acesso 
+app.use(express.json()) //middleware para verificar tipagens (robustez)
+app.use("/api" ,routes);
+
+export default app;
